6d5a6c6c62cf22e64c23235d5a90478c2913e3b7
Naveen Kumar <naveen@devqailabs.com>
2026-05-02 19:25:33 +0530

    Added PWA Changes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './i18n/i18n.js'
 import './index.css'
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW({ immediate: true })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
         react(),
         VitePWA({
             registerType: 'autoUpdate',
-            includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+            includeAssets: ['ganesh.png'],
             manifest: {
                 name: 'Moi Vibaram',
                 short_name: 'MoiVibaram',
